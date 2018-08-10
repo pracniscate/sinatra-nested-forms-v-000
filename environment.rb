c3bbac5b940d4f813fd552770fe2e84bab607e35
Lorena <lorena.giddens@outlook.com>
2018-08-10 00:36:27 +0000

    Add gems and establish connection to the database

--- environment.rb
+++ environment.rb
@@ -3,6 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['Sinatra_ENV']}.sqlite"
+)
+
 require 'sinatra/base'
 require 'sinatra/reloader'
 
