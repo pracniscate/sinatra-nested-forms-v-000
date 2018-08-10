896ad30b8975a9d15330c130c3caee54a01516ce
Lorena <lorena.giddens@outlook.com>
2018-08-10 01:43:19 +0000

    Add routes in the controller

--- app.rb
+++ app.rb
@@ -3,23 +3,34 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-    get '/' do
-      erb :root
+    # get '/' do
+    #   erb :root
+    # end
+    #
+    # get '/new' do
+    #   erb :'pirates/new'
+    # end
+    #
+    # post '/pirates' do
+    #   @pirate = Pirate.new(params[:pirate])
+    #
+    #   params[:pirate][:ships].each do |details|
+    #     Ship.new(details)
+    #   end
+    #
+    #   @ships = Ship.all
+    #
+    #   erb :'pirates/show'
+    # end
+
+    get '/pirates' do
+      @pirates = Pirate.all # data structure: array
+
+      erb :'pirates/index'
     end
 
-    get '/new' do
-      erb :'pirates/new'
-    end
-
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-
-      @ships = Ship.all
+    get '/pirates/:id' do # dynamic segment
+      @pirate = Pirate.find(params["id"])
 
       erb :'pirates/show'
     end
